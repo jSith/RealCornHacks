ba116004df539496df6102b764702f5e4cd12d82
Joe Cowman <joe.cowman@huskers.unl.edu>
2019-01-20 04:44:41 -0600

    Add models

---
+++ Cornhacks2019/Cornhacks2019/Models/Preference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cornhacks2019.Models
+{
+    public class Preference
+    {
+        public bool IsBeginner { get; set; }
+        public List<string> Topics { get; set; }
+        public List<string> Languages { get; set; }
+        public List<SizeEnum.Size> Sizes { get; set; }
+    }
+}
---
+++ Cornhacks2019/Cornhacks2019/Models/Repository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cornhacks2019.Models
+{
+    public class Repository
+    {
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Description { get; set; }
+
+        public int NumberOfContributors { get; set; }
+         
+        public List<Issue> Issues { get; set; }
+
+        public List<string> Languages { get; set; }
+
+        public string Url { get; set; }
+
+        public Owner Owner { get; set; }
+    }
+
+    public class Owner
+    {
+        public string Login { get; set; }
+
+        public string Avatar_Url { get; set; }
+    }
+}
