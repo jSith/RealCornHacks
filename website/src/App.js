9c1df453c897e1e605fc3512dee708b168d0e40c
Joe Cowman <joe.cowman@huskers.unl.edu>
2019-01-19 21:56:09 -0600

    Implement react router

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import CreateAccount from './components/CreateAccountComponent';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <CreateAccount />
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <CreateAccount />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+const Home = () => <div>Click here to <Link to="/subscribe/">Subscribe</Link></div>;
+const Subscribe = () => <CreateAccount />;
+
+const AppRouter = () => (
+    <Router>
+        <div>
+            <Route exact path="/" component={Home} />
+            <Route path="/subscribe/" component={Subscribe} />
+        </div>
+    </Router>
+);
+
+export default AppRouter;
