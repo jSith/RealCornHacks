9a6f79539e01626ecb9e164fbca5428908e45f6e
jSith <jwilmasmith@gmail.com>
2019-01-19 21:07:22 -0600

    sending test emails

--- CornHacks2019/Engines/EmailEngine.cs
+++ CornHacks2019/Engines/EmailEngine.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Cornhacks2019.Models;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 
 namespace Cornhacks2019.Engines
 {
     public class EmailEngine
     {
+        public void CreateEmail(Dictionary<Repository, List<Issue>> dictionary)
+        {
+
+        }
+
+        
+        public void SendEmail(User user)
+        {            
+            var basicCredential = new NetworkCredential("cornhacksteamtwo@gmail.com", "testingpass1");
+            MailMessage mail = new MailMessage("cornhacksteamtwo@gmail.com", "jharkendorff@gmail.com");
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = basicCredential;
+            client.Host = "smtp.gmail.com";
+
+            mail.IsBodyHtml = true;
+            mail.Subject = "this is a test email.";
+            mail.Body = "this is my test email body";
+            client.Send(mail);
+        }
+
+        public string GetTarget(User user)
+        {
+            return user.Email;
+        }
     }
 }
