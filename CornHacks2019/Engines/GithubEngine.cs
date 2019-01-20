c892d600f2b2d9ad60b2fd8e2cc6e0b6ff2065fc
jSith <jwilmasmith@gmail.com>
2019-01-19 21:44:10 -0600

    Added interfaces for unity

--- CornHacks2019/Engines/GithubEngine.cs
+++ CornHacks2019/Engines/GithubEngine.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using CornHacks2019.Interfaces.EngineInterfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Cornhacks2019.Engines
 {
-    class GithubEngine
+    class GithubEngine : IGithubEngine
     {
     }
 }
