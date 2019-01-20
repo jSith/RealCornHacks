59e848d6181f9163c5a3db8f248fa5753bb5935f
jSith <jwilmasmith@gmail.com>
2019-01-19 18:10:57 -0600

    Make test proj

---
+++ Cornhacks2019.Tests/CornHacks2019.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CornHacks2019; 
+
+namespace CornHacks2019.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
