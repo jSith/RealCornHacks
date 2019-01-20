2f77c650f8c7e0e8dc353ef24bcd377788abbabb
John Harkendorff <jharkendorff@gmail.com>
2019-01-19 23:27:00 -0600

    Failing unit test to debug later

--- NUnitTestProject1/UserAccessorTest.cs
+++ NUnitTestProject1/UserAccessorTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Cornhacks2019.Accessors;
+using Cornhacks2019.Models;
 using NUnit.Framework;
 
 namespace Tests
@@ -14,6 +15,34 @@ namespace Tests
         public void UserAccessorTest()
         {
             var response = _userAccessor.Select(); 
+        }
+
+        [Test]
+        public void TestInsert()
+        {
+            UserAccessor userAccessor = new UserAccessor();
+
+            User user = new User();
+            user.Email = "email@gmail.com";
+            user.Password = "password123";
+            user.Preference.IsBeginner = true;
+            user.Preference.Languages = new List<string>()
+            {
+                "Python",
+                "C#",
+                "Ruby"
+            };
+            user.Preference.Topics = new List<string>()
+            {
+                "Machine Learning",
+                "Database"
+            };
+            user.Preference.Sizes = new List<SizeEnum.Size>()
+            {
+                SizeEnum.Size.Medium
+            };
+
+            userAccessor.Insert(user);
         }
     }
 }
