93d2c276d98545ea7f8136d440d9aa9504d59eff
jSith <jwilmasmith@gmail.com>
2019-01-20 02:17:27 -0600

    Scaffold

--- CornHacks2019/Engines/SubscriptionEngine.cs
+++ CornHacks2019/Engines/SubscriptionEngine.cs
@@ -1,8 +1,27 @@
-﻿using CornHacks2019.Interfaces.EngineInterfaces;
+﻿using Cornhacks2019.Accessors;
+using Cornhacks2019.Models;
+using CornHacks2019.Interfaces.EngineInterfaces;
 
 namespace Cornhacks2019.Engines
 {
     public class SubscriptionEngine : ISubscriptionEngine
     {
+        private UserAccessor _userAccessor; 
+
+        public SubscriptionEngine(UserAccessor userAccessor)
+        {
+            _userAccessor = userAccessor; 
+        }
+        
+        public User Subscribe()
+        {
+            return _userAccessor.AddUser(User); 
+
+        }
+
+        public User Unsubscribe()
+        {
+            return _userAccessor.DeleteUser(User); 
+        }
     }
 }
