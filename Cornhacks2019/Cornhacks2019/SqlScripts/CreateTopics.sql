4577bbc91285f717a20a6b2b7247b18b33457f48
Joe Cowman <joe.cowman@huskers.unl.edu>
2019-01-20 05:00:51 -0600

    Copy sql scripts

---
+++ Cornhacks2019/Cornhacks2019/SqlScripts/CreateTopics.sql
@@ -0,0 +1,4 @@
+INSERT INTO Topic (TopicName) VALUES ("Machine Learning"); 
+INSERT INTO Topic (TopicName) VALUES ("API"); 
+INSERT INTO Topic (TopicName) VALUES ("Artificial Intelligence"); 
+INSERT INTO Topic (TopicName) VALUES ("Robotics"); 
