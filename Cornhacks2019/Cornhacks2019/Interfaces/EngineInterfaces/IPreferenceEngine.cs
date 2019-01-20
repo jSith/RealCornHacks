c456d3c0562d2820f0436d4cf358035f9d4ef898
Joe Cowman <joe.cowman@huskers.unl.edu>
2019-01-20 04:46:04 -0600

    Add interfaces

---
+++ Cornhacks2019/Cornhacks2019/Interfaces/EngineInterfaces/IPreferenceEngine.cs
@@ -0,0 +1,13 @@
+﻿using Cornhacks2019.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CornHacks2019.Interfaces.EngineInterfaces
+{
+    public interface IPreferenceEngine
+    {
+        Preference GetAllOptions(); 
+    }
+}
